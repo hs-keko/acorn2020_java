9f07325f46488734ddd57e8de2d247160c95e384
hsko <manic9075@gmail.com>
2021-05-04 12:13:47 +0900

    Rect.java 설명 추가

--- Step04_Class/src/test/mypac/Rect.java
+++ Step04_Class/src/test/mypac/Rect.java
@@ -7,8 +7,9 @@ public class Rect {
 	public int height; //높이
 	
 	//사각형의 면적을 리턴해주는 non static 메소드 
-	public int getArea() {
+	public int getArea() {	// 이 method 를 실행하면 int 값을 리턴해준다.
 		int area=this.width * this.height;
-		return area;
+		return area;	// return 다음에 반드시 int 값이 와야 문법에 맞는다.   
+						// return true(x), return "김구라"(x), return 10.1(x)
 	}
 }
