64f18746f25387268fbedd30d8a64d09710fd75e
hsko <manic9075@gmail.com>
2021-05-04 12:09:36 +0900

    Member.java 수정

--- Step04_Class/src/test/mypac/Member.java
+++ Step04_Class/src/test/mypac/Member.java
@@ -7,7 +7,7 @@ public class Member {
 	public String addr;
 	
 	//non static 메소드 정의하기
-	public void showInfo() {
+	public void showInfo() {	// void : 어떤 값도 리턴하지 않을거니 받아갈 생각도 하지마!
 		System.out.println(this.num+" | "+this.name+" | "+this.addr);	
 	
 	}
